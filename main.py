dc128db3eb0428e0795bea9fc932833a358c4a83
Philippe Van Borsel <philippe.van_borsel@teledisnet.be>
2019-02-28 11:41:14 +0100

    :construction:Add project files
    Create 'queue.txt' & 'crawled.txt' files if they don't exist.

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
 import os
 
+# Create a new file
+def writeFile(path, data):
+    f = open(path,'w')
+    f.write(data)
+    f.close()
+
+# Create directories for the crawling project
 def createDir(directory):
     if not os.path.exists(directory):
         print('Creating project directory' + directory)
         os.makedirs(directory)
+
+# Create crawling files 'QUEUE & CRAWLED'
+def createDataFiles(projectName,baseUrl):
+    queue = projectName + 'queue.txt'
+    crawled = projectName + 'crawled.txt'
+    if not os.path.isfile(queue):
+        writeFile(queue, baseUrl)
+    if not os.path.isfile(crawled):
+        writeFile(crawled, '')
