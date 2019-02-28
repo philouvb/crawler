2efb593c04dc90ed26eb77e2cf6263da9ab1da72
Philippe Van Borsel <philippe.van_borsel@teledisnet.be>
2019-02-28 18:50:39 +0100

    :construction:Add 'link_Finder' class
    Class is use to parse url and find each opening '<a>' tag and take the 'href' which is just behind
    it.

---
+++ link_finder.py
@@ -0,0 +1,24 @@
+from html.parser import HTMLParser
+from urllib import parse
+
+class LinkFinder(HTMLParser):
+
+    def __init__(self, baseUrl, pageUrl):
+        super.__init__()
+        self.baseUrl = baseUrl
+        self.pageUrl = pageUrl
+        self.links = set()
+
+    # When calling HTMLPaser feed, this function is called when opening tag
+    def handle_starttag(self, tag, attrs):
+        if tag == 'a':
+            for (attribute, value) in attrs:
+                if attribute == 'href':
+                    url = parse.urljoin(self, baseUrl, value):
+                    self.links.add(url)
+
+    def pageLinks(self):
+        return self.links
+
+    def error(self, message):
+        pass
