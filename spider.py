706562895f366cbf2426de6e572999a49acfa8ae
Philippe Van Borsel <philippe.van_borsel@teledisnet.be>
2019-03-02 14:21:58 +0100

    :construction: Add Spider class. Class whitch have to crawl the destination website.

---
+++ spider.py
@@ -0,0 +1,25 @@
+from urllib.request import urlopen
+from LinkFinder import LinkFinder
+from general import *:
+
+
+class Spider:
+
+    # class variables ( shared among all instances )
+    project_name = ''
+    base_url =''
+    domain_name =''
+    queue_file =''
+    crawled_file =''
+    queue = set()
+    crawled = set()
+
+
+    def __init__(self, project_name, base_url, domain_name):
+        Spider.project_name = project_name
+        Spider.base_url = base_url
+        Spider.domain_name = domain_name
+        Spider.queue_file = Spider.project_name + '/queue.txt'
+        Spider.crawled_file = Spider.project_name + '/crawled.txt'
+        self.boot()
+        self.crawl_page('First spider', Spider.base_url)
